2d5175a30d205fac5d0b385d7fc4282d47508884
geekawscloud <geek.awscloud@gmail.com>
2017-04-28 13:57:35 +0530

    Update createbucket.php

--- createbucket.php
+++ createbucket.php
@@ -5,7 +5,7 @@
 include 'connecttoaws.php';
 
 // Create a unique bucket name
-$bucket = uniqid("acloudguru", true);
+$bucket = uniqid("cloudperhour", true);
 
 // Create our bucket using our unique bucket name
 $result = $client->createBucket(array(
@@ -13,8 +13,8 @@ $result = $client->createBucket(array(
 ));
 
 //HTML to Create our webpage
-echo "<h1 align=\"center\">Hello Cloud Guru!</h1>";
-echo "<div align = \"center\"><img src=\"https://s3-eu-west-1.amazonaws.com/acloudguru/logo.png\"></img></div>";
+echo "<h1 align=\"center\">Hello Cloud Experts!</h1>";
+echo "<div align = \"center\"><img src=\"https://s3.amazonaws.com/cloudperhour/cloudperhour.jpg"></img></div>";
 echo "<h2 align=\"center\">You have successfully created a bucket called {$bucket}</h2>";
 echo "<div align=\"center\"><a href=\"createfile.php?bucket=$bucket\">Click Here to Continue</a></div>";
 ?>
