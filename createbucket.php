6da3fef63dd3b497b0d84b7b2f9ae8b8c9358c63
geekawscloud <geek.awscloud@gmail.com>
2017-04-28 15:39:53 +0530

    Update createbucket.php

--- createbucket.php
+++ createbucket.php
@@ -14,7 +14,7 @@ $result = $client->createBucket(array(
 
 //HTML to Create our webpage
 echo "<h1 align=\"center\">Hello Cloud Experts!</h1>";
-echo "<div align = \"center\"><img src=\"https://s3.amazonaws.com/cloudperhour/cloudperhour.jpg"></img></div>";
+echo "<div align= \"center\"><img src='https://s3.amazonaws.com/cloudperhour/cloudperhour.jpg'/></div$
 echo "<h2 align=\"center\">You have successfully created a bucket called {$bucket}</h2>";
-echo "<div align=\"center\"><a href=\"createfile.php?bucket=$bucket\">Click Here to Continue</a></div>";
+echo "<div align=\"center\"><a href=\"createfile.php?bucket=$bucket\">Click Here to Continue</a></div$
 ?>
